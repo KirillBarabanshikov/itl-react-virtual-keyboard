40ea2f6df2dd884558cb94571e066d6e9a9afac7
Kirill <kirillbar24@gmail.com>
2025-04-18 17:55:25 +0400

    version 0.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,19 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.js', // Явно указываем файл
+      file: 'dist/index.js',
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
     },
     {
-      file: 'dist/index.esm.js', // Явно указываем файл
+      file: 'dist/index.esm.js',
       format: 'esm',
       sourcemap: true,
     },
@@ -32,6 +33,26 @@ export default {
       modules: false,
       sourceMap: true,
       minimize: true,
+      plugins: [
+        require('postcss-url')({
+          url: (asset) => {
+            return `/assets/icons/${asset.url.split('/').pop()}`;
+          },
+        }),
+      ],
+    }),
+    copy({
+      targets: [
+        {
+          src: 'src/assets/fonts/*',
+          dest: 'dist/assets/fonts',
+        },
+        {
+          src: 'src/assets/icons/*',
+          dest: 'dist/assets/icons',
+        },
+      ],
+      copyOnce: true,
     }),
   ],
 };
